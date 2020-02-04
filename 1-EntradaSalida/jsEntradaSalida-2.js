b33d7ac7cd1ef66c5194bf8e6393977cec42e235
lmiralles0 <lucianomiralles96@gmail.com>
2020-02-04 00:55:37 -0300

    Ejercicios primera clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,15 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+// Tambien se puede poner:
+// var nombre
+// alert(prompt("Ingrese su nombre"));
+// notese que si se borra la "function mostar" aparece el cuadro de entrada
 function mostrar()
 {
-	
+    var nombre
+    
+    nombre = prompt("Ingrese su nombre");
+
+    alert(nombre);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,14 @@
 /*Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+//se utiliza el "docume..."" para dar entrada
+//alert se puede agregar una variable con un texto o paint
 function mostrar()
 {
-	
+    var nombre
+    
+    nombre = document.getElementById("elNombre").value;
+
+    alert("Su nombre es " + nombre);
 }
 
 
