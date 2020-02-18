cd2f7bcfeea87e1c94fbc7c2e3cc3466bb5424a1
alumno <alumno@utnfra.com.ar>
2020-02-18 20:34:43 -0300

    Ejercicios tercera clase :(

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,32 @@
 function mostrar()
 {
+  var numeroIngresado // asignar numeros que se ingresan
+	var contador = 0; // para contar en bucle (cuantas veces giro)
+	var acumulador = 0; // para sumar los numeros ingresados
+  var promedio // para guardar el promedio
 
-	var contador=0;
-	var acumulador=0;
 
 
+  do {
+      //Ingresar el numero
+      numeroIngresado = prompt("Ingrese un numero ");
+      numeroIngresado = parseInt(numeroIngresado);
+      while (isNaN(numeroIngresado)) {
+        numeroIngresado = prompt("Lo que ingresaste no era un numero");
+        numeroIngresado = parseInt(numeroIngresado);
+     }
+      //En esta parte del codigo sabemos que el numero es correcto
+      contador++; // contador = contador + 1
+      acumulador = acumulador + numeroIngresado;
+      console.log("El contador es : "+contador);
+      console.log("Se encuentra acumulando : "+ acumulador);
+      } while (contador < 5) {
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+    }
+//Muestra el acumulador
+document.getElementById('suma').value = acumulador;
+promedio = acumulador / contador;
+// Mostramos el promedio
+document.getElementById('promedio').value = acumulador/5;
 
 }//FIN DE LA FUNCIÓN
