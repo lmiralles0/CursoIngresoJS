f932e0be1a4d227000c4f08ceb8b78ca34f2bf64
lmiralles0 <lucianomiralles96@gmail.com>
2020-02-22 20:54:21 -0300

    Ejercicios tercer clase :(

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -15,45 +15,55 @@ function mostrar()
   var respuesta="si";
 
 	while(respuesta != "no"){
-    numeroIngresado = prompt("Ingrese un numero ");
-    numeroIngresado = parseInt(numeroIngresado);
-    while (isNaN(numeroIngresado)){
-        numeroIngresado = prompt("Lo que ingresaste no era un numero");
-        numeroIngresado = parseInt(numeroIngresado);
-      }
-  if (numeroIngresado > 0){
-    //es postivio
-    acumuladorPositivo = acumuladorPositivo + numeroIngresado;
-    cantidadPositivo ++;
-    var resto = numeroIngresado % 2;
-    if (resto == 0){
-      // es un numero par
-      cantidadNumerosPares++;
-    }
-  }else if(numeroIngresado < 0){
-    // es negativo
-    acumuladorNegativo = acumuladorNegatvo + numeroIngresado;
-    cantidadNegativos ++;
-  }else{
-    cantidadCeros ++;
-  }
-   contador ++;
-   respuesta = prompt("Desesa continuar? Si desea continuar escriba 'si'");
+    
+          numeroIngresado = prompt("Ingrese un numero ");
+          numeroIngresado = parseInt(numeroIngresado);
+          while (isNaN(numeroIngresado)){
+                  numeroIngresado = prompt("Lo que ingresaste no era un numero");
+                  numeroIngresado = parseInt(numeroIngresado);
+          }
+          
+          if (numeroIngresado > 0){
+                   //es postivio
+                   acumuladorPositivos = acumuladorPositivos + numeroIngresado;
+                   cantidadPositivos ++;
+                   var resto = numeroIngresado % 2;
+                   if (resto == 0){
+                          // es un numero par
+                           cantidadNumerosPares++;
+                   }
+                  console.log("Numero ingresado: " + numeroIngresado);  
+
+          }else if (numeroIngresado < 0){
+                  // es negativo
+                  acumuladorNegativos = acumuladorNegativos + numeroIngresado;
+                  cantidadNegativos ++;
+          }else{
+                  cantidadCeros ++;
+          }
+
+
+          contador ++;
+          respuesta = prompt("Desesa continuar? Si desea continuar escriba 'no'");
 
 }//termina el bucle
+
+
+//Se calculan los promedio
 
 promedioNegativos = acumuladorNegativos / cantidadNegativos;
 promedioPositivos = acumuladorPositivos / cantidadPositivos;
 diferenciaNegativosPositivos = promedioPositivos - promedioNegativos;
 
-document.write("El total de numeros positivos es de: " + acumuladorPositivos + "\n" +
-               "El total de numeros negativos es de: " + acumuladorNegativos + "\n" +
-               "El promedio de numeros negativos es de: " + promedioNegativos + "\n" +
-               "El promedio de numeros positivos es de: " + promedioPositivos + "\n" +
-               "La cantidad de ceros es de: " + cantidadCeros + "\n" +
+document.write("El total de numeros positivos es de: " + acumuladorPositivos + "<br>" +
+               "El total de numeros negativos es de: " + acumuladorNegativos + "<br>" +
+               "El promedio de numeros negativos es de: " + promedioNegativos + "<br>" +
+               "El promedio de numeros positivos es de: " + promedioPositivos + "<br>" +
+               "La cantidad de ceros es de: " + cantidadCeros + "<br>" +
                " La diferencia entre Nº positivos y negastivos es de: " + diferenciaNegativosPositivos);
 
 
 
-
+//si en documentwrite se imprime "\n" se imprime uno al lado del otro 
+//para poner uno debajo del otro "<br>"
 }//FIN DE LA FUNCIÓN
