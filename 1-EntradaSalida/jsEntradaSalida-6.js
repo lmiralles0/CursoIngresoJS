af66fe85c4da075510002bdcfce3b1d2b5412faf
lmiralles0 <lucianomiralles96@gmail.com>
2020-02-24 23:28:45 -0300

    Ejercicios primer clase

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+  var numUno;
+  var numDos;
+  var resultado;
 
+  numUno = parseInt(document.getElementById("numeroUno").value);
+  numDos = parseInt(document.getElementById("numeroDos").value);
+  resultado = (numUno + numDos);
+
+  alert ("El resultado de la suma es " + resultado + ".");
 }
 
