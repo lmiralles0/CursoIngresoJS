3fc3598d1a888062ac146c3c29f7cdf9631bd304
lmiralles0 <lucianomiralles96@gmail.com>
2020-02-26 02:28:45 -0300

    Ejercicios primer clase

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,40 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+var numUno;
+var numDos;
+var resultado;
+
 function sumar()
 {	
-	
+    numUno = parseInt(document.getElementById("numeroUno").value);
+    numDos = parseInt(document.getElementById("numeroDos").value);
+    resultado = (numUno + numDos);
+    alert("El resultado de la suma es " + resultado + ".");
 }
 
 function restar()
 {
-	
+    numUno = parseInt(document.getElementById("numeroUno").value);
+    numDos = parseInt(document.getElementById("numeroDos").value);
+    resultado = (numUno - numDos);
+    alert("El resultado de la resta es " + resultado + ".");
 }
 
 function multiplicar()
 { 
-	
+	numUno = parseInt(document.getElementById("numeroUno").value);
+    numDos = parseInt(document.getElementById("numeroDos").value);
+    resultado = (numUno * numDos);
+    alert("El resultado de la multiplicacion es " + resultado + ".");
 }
 
 function dividir()
 {
-	
+	numUno = parseInt(document.getElementById("numeroUno").value);
+    numDos = parseInt(document.getElementById("numeroDos").value);
+    resultado = (numUno / numDos);
+    alert("El resultado de la division es " + resultado + ".");
 }
 
