1e9b4c61f0489036284100bf5cccba9471b4608e
lmiralles0 <lucianomiralles96@gmail.com>
2020-02-29 23:02:59 -0300

    Ejercicios primer clase

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var precio;
+    var descuento;
+    var resultado;
+
+    precio = parseFloat(document.getElementById("importe").value);
+    descuento = parseFloat(precio * 25 / 100);
+    resultado = (precio - descuento);
+    document.getElementById("resultado").value = resultado; 
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+  var sueldo;
+  var aumento;
+  var resultado;
 
-	
+  sueldo = parseInt(document.getElementById("sueldo").value);
+  aumento = parseFloat(sueldo * 10 / 100);
+  resultado = (sueldo + aumento);
+  document.getElementById("resultado").value = resultado;
+   	
 }
