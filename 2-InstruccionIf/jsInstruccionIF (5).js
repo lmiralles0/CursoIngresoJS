e5ef45fb68603f22d64af1755602588cfe3e2083
lmiralles0 <60629914+lmiralles0@users.noreply.github.com>
2020-03-02 12:22:50 -0300

    Update jsInstruccionIF (5).js

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -5,10 +5,8 @@ var edad;
 
 edad = parseInt(document.getElementById("edad").value);
 
-if(edad > 17){
+if(edad > 17 || edad < 13){
     alert("Usted NO es adolecente.");
-} else if (edad < 13){
-    alert("Usted NO es adolecente.");
-}
+} 
 
 }//FIN DE LA FUNCIÓN
