2ab5b58b83c579cb6868ca16e77b0c3be2f9e01f
alumno <alumno@utnfra.com.ar>
2020-03-02 22:08:37 -0300

    Ejercicios cuarta clase

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,5 +1,17 @@
-function mostrar()
-{
+function mostrar(){
+  var numero;
+  var contadorDivisores = 0;
+
+  numero = parseInt(prompt("Ingrese un numero : "));
+
+  for (i = 1; i <= numero; i++){
+    if(numero % 1 == 0){
+      console.log(numero);
+      contadorDivisores++;
+
+    }
+  console.log(contadorDivisores);
+  }
 
 
 
