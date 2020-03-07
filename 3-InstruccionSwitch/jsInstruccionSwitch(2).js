1297bb3bec94077072466b4eaf8598f9b5f6d81e
lmiralles0 <lucianomiralles96@gmail.com>
2020-03-07 13:34:29 -0300

    Ejercicios tercer clase

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,10 +1,23 @@
-function mostrar()
-{
+function mostrar(){
+
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
 
+switch(mesDelAño){
+    default:        
+        alert("Falta para el invierno.");
+        break;
+    case "Julio":
+    case "Agosto":    
+        alert("Abrigate que hace frio.");
+        break;
+    case "Septiembre":
+    case "Noviembre":
+    case "Diciembre":        
+        alert("Ya pasamos el frio, ahora calor!!!.");
+        break;
+    }
 
 
 
